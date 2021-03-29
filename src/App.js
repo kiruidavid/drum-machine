511388889c4eba0c6055316ad018b90142b2328f
unknown <david.wendot@gmail.com>
2021-03-29 16:08:57 +0300

    First commit to the drum machine app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css' 
+const data = [
+  { id: 'snare', letter: 'Q', 
+  src: 'https://www.myinstants.com/media/sounds/snare.mp3' }, 
+  { id: 'bass 1', letter: 'W', 
+  src: 'https://www.myinstants.com/media/sounds/bass-drum.mp3' }, 
+  { id: 'bongo', letter: 'E', 
+  src: 'http://tipiwiki.free.fr/snd/Percussion(4e)2.wav' }, 
+  { id: 'tom tom', letter: 'A', 
+  src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' }, 
+  { id: 'bass 3', letter: 'S', 
+  src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' }, 
+  { id: 'shotgun', letter: 'D', 
+  src: 'http://david.guerrero.free.fr/Effects/ShotgunReload.wav' }, 
+  { id: 'high hat', letter: 'Z',
+   src: 'http://www.denhaku.com/r_box/tr707/closed.wav' }, 
+  { id: 'drum hit', letter: 'X', 
+  src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'  },
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   { id: 'laser', letter: 'C', 
+   src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'  },
+] 
+class  Drumpad extends Component {  
+  handleClick = () => {
+    this.audio.play() 
+    this.props.handleDisplay(this.props.id)
+  }
+  render(){
+  return(
+    <div className="drum-pad col p-3 border bg-info"
+     id={this.props.id} 
+     onClick={this.handleClick}> 
+      <p>{this.props.letter}</p> 
+      <audio  
+      ref={ref => this.audio = ref}
+      className="clip"
+      src={this.props.src}
+      id={this.props.letter} 
+      > 
+    
+      </audio>
+
     </div>
-  );
+  ) 
+  }
 }
+class App extends Component{ 
+  constructor(props){
+    super(props) 
+    this.state = {
+      display: ''
+    }
+  }  
+  handleDisplay = display => {
+    this.setState({display: display})
+  }
+  
+  render(){
+    return(
+      <div className="container">
+        <h1>Drum Machine</h1> 
+        <div id="drum-machine">  
+          <div id="display"> 
+            <h5>{this.state.display}</h5>
+          </div>  
+          <div className="container">
+          <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+ 
+            
+            
+              {data.map(d => (
+                <Drumpad id={d.id} 
+                letter={d.letter} 
+                src={d.src} 
+                handleDisplay={this.handleDisplay}
+                />
+              ))} 
+              
+            
+            </div>
 
-export default App;
+          </div>
+
+
+ 
+
+
+
+
+          </div>
+
+
+        </div>
+      
+    )
+  }
+} 
+export default App
